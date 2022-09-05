90e3f535aebb0961c9f0e85988e53f90a57ff534
just9nnnn paul <1365762824@qq.com>
2022-09-05 11:19:59 +0800

    Add files via upload

--- practice_03/test.c
+++ practice_03/test.c
@@ -1,8 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <math.h>
-int main(void)
+int main()
 {
-	printf("\a\a\a\a");
+	int input = 0;
+	printf("想成为成为大师？\n");
+	printf("你会认真学习吗？(1/0)");
+	scanf(" %d", &input);
+	if (input == 1)
+		printf("加油!\n");
+	else
+		printf("你没有选择！\n");
 	return 0;
 }

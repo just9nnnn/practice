502ccd2bae313968edf27773753054d70113aa53
SQ eggs <1365762824@qq.com>
2022-09-06 16:53:37 +0800

    Add files via upload

--- practice_11/test.c
+++ practice_11/test.c
@@ -5,13 +5,13 @@ int main()
 	int age = 0;
 	printf("请输入你的年龄:>");
 	scanf("%d", &age);
-	if (age < 8)
+	if (age <= 8)
 		printf("童年\n");
-	else if (8 <= age && age <= 18)
+	else if (age > 8 && age <= 18)
 		printf("青少年\n");
-	else if (18 < age && age <= 35)
+	else if (age > 18 && age <= 35)
 		printf("青年\n");
-	else if (35 < age && age <= 60)
+	else if (age > 35 && age <= 60)
 		printf("中年\n");
 	else
 		printf("老年\n");

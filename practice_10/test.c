e3f984d8ff19c1d513f6b28f5f29540146763d61
SQ eggs <1365762824@qq.com>
2022-09-06 14:02:11 +0800

    Add files via upload

--- practice_10/test.c
+++ practice_10/test.c
@@ -10,6 +10,9 @@ struct man
 int main()
 {
 	struct man ls = { "SQ eggs", 23, "male","13600008714" };
-	printf("姓名:%s 年龄:%d 性别:%s 电话:%s",ls.name, ls.age, ls.sex, ls.tell);
+	struct man* pls = &ls;
+	printf("1: 姓名:%s 年龄:%d 性别:%s 电话:%s\n",ls.name, ls.age, ls.sex, ls.tell);
+	printf("2: 姓名:%s 年龄:%d 性别:%s 电话:%s\n",(*pls).name, (*pls).age, (*pls).sex, (*pls).tell );
+	printf("3: 姓名:%s 年龄:%d 性别:%s 电话:%s\n",pls->name, pls->age, pls->sex, pls->tell);
 	return 0;
 }

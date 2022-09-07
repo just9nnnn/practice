1b4daea5a296a341e395cb733d15bb11c81ad2d8
SQ eggs <1365762824@qq.com>
2022-09-07 12:27:11 +0800

    Add files via upload

---
+++ practice_16_test2/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	printf("输入一个数i，求<=i的数的阶乘的和:>");
+	scanf("%d", &i);
+	int a = 1;
+	int b = 1;
+	int c = 0;
+	for(;b < i+1; b++)
+	{
+		a = a * b;
+		c = c + a;
+	}
+	printf("%d", c);
+	return 0;
+}

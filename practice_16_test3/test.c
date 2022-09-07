cefac76d54a8f483b3eb0752dd26ac53946d33ea
SQ eggs <1365762824@qq.com>
2022-09-07 14:40:09 +0800

    Add files via upload

---
+++ practice_16_test3/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int k = 7;//需要寻找的数
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };//要搜寻的数组
+	int sz = sizeof(arr) / sizeof(arr[0]);//数组中的元素个数
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (mid < k - 1)
+		{
+			left = mid + 1;
+		}
+		else if (mid > k - 1)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("找到了，这个数是:%d", mid + 1);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("无法找到\n");
+	}
+	return 0;
+}

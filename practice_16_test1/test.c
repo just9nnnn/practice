fb860d93d6a90c74b4052d3c6b0728de3984aa13
SQ eggs <1365762824@qq.com>
2022-09-07 12:00:46 +0800

    Add files via upload

---
+++ practice_16_test1/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	printf("输入一个数，求它的阶乘:>");
+	scanf("%d", &i);
+	int a = 1;
+	int b = 1;
+	for (; b < i+1; b++)
+	{
+		a = a * b;
+	}
+	printf("%d ", a);
+	return 0;
+}

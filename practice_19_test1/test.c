28250e0a458e4dc2eaa9891b8edd1509ea4ad69f
SQ eggs <1365762824@qq.com>
2022-09-11 22:21:52 +0800

    Add files via upload

---
+++ practice_19_test1/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int my_strlen(char* str)
+{
+	if (*str != '\0')
+	{
+		return 1 + my_strlen(str + 1);
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+	char arr[] = "Hello world!";
+	printf("%d",my_strlen(arr));
+	return 0;
+}
